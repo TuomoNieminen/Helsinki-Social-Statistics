097fc44e95d481aabd1184bdb6492c95ae46b86a
TuomoNieminen <tuomo.a.nieminen@helsinki.fi>
2016-08-27 00:18:52 +0300

    no real change

--- functions.R
+++ functions.R
@@ -1,4 +1,7 @@
 
+# load JYT data from github repo
 get_data <- function() read.table("https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Social-Statistics/master/data/JYTOPKYS-data.txt", sep="\t", header=TRUE)
+
+# open metadata in a browser window
 meta <- function() browseURL("https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Social-Statistics/master/data/JYTOPKYS-meta.txt")
 
