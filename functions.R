0ace59f804c9cb3b993e38e539ec4119ca053e01
TuomoNieminen <tuomo.a.nieminen@helsinki.fi>
2016-08-26 22:04:56 +0300

    rename metafunc

--- functions.R
+++ functions.R
@@ -1,3 +1,4 @@
 
 get_data <- function() read.table("https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Social-Statistics/master/data/JYTOPKYS-data.txt", sep="\t", header=TRUE)
-get_meta <- function() browseURL("https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Social-Statistics/master/data/JYTOPKYS-meta.txt")
+meta <- function() browseURL("https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Social-Statistics/master/data/JYTOPKYS-meta.txt")
+
