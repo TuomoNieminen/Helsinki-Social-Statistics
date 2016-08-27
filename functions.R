adf93f0965c3fe67f14b468b3cda5330de9cb3ed
TuomoNieminen <tuomo.a.nieminen@helsinki.fi>
2016-08-27 23:20:16 +0300

    updated the data urls

--- functions.R
+++ functions.R
@@ -1,7 +1,7 @@
 
 # load JYT data from github repo
-get_data <- function() read.table("https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Social-Statistics/master/data/JYTOPKYS-data.txt", sep="\t", header=TRUE)
+get_data <- function() read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS-data.txt", sep="\t", header=TRUE)
 
 # open metadata in a browser window
-meta <- function() browseURL("https://raw.githubusercontent.com/TuomoNieminen/Helsinki-Social-Statistics/master/data/JYTOPKYS-meta.txt")
+meta <- function() browseURL("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS-meta.txt")
 
